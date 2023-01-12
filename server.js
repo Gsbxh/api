9b92e27205356f626ba7f87d9e8606d898eed29b
Gsbxh <101274187+Gsbxh@users.noreply.github.com>
2023-01-12 12:36:28 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var axios = require('axios')
+const { insta } = require('./X0/ig.js')
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+
+app.get("/insta", function (req, res)  {
+  console.log("request coming in...");
+  insta(`${req.query.url}`).then(({ creator , url }) => {
+                    result = {
+                      "status" : "true" ,
+                      "creator" : creator,
+                      "media" : {
+                      "url" : url 
+                      }
+                    }
+                      res.status(200).json( result );
+  
+                  })
+})
+
+ 
+
+const server = http.createServer(app);
+server.listen(port);
+
